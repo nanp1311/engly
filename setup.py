19953345940d28da23b6bdfb97f639ef020d4097
nanp <fzd65553@kwansei.ac.jp>
2023-11-30 14:58:30 +0900

    Anly

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="Anly",
     version='1.0',
-    description='英語学習アプリ',
+    description='English Learning Application',
     author='nanp1311',
     author_email='pengin.na11@gmail.com',
-    url='https://github.com/nanp1311/',
+    url='https://github.com/nanp1311/Anly',
+     packages=find_packages(),
+    entry_points="""
+      [console_scripts]
+      anly = Anly.main:hello
+    """,
 )
