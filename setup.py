2eb67cf610c8e6be07912c778f2b1eb7d13949e4
nanp <fzd65553@kwansei.ac.jp>
2023-11-30 14:33:57 +0900

    create Anly

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Anly",
+    version='1.0',
+    description='英語学習アプリ',
+    author='nanp1311',
+    author_email='pengin.na11@gmail.com',
+    url='https://github.com/nanp1311/',
+)
